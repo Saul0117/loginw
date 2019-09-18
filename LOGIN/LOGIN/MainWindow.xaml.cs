6844725f742b2a425f8ea07a9256ecb622ccf277
Probiazº <alexy-tobiaz2002@hotmail.com>
2019-09-18 12:31:45 -0500

    xd

---
+++ LOGIN/LOGIN/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LOGIN
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Title = "Login con puerta abierta";
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Iniciar_Click(object sender, RoutedEventArgs e)
+        {
+            if(Nombre.Text =="juancho"&& Contraseña.Text=="123tamarindo")
+            {
+                Window1 form = new Window1();
+                    form.ShowDialog();
+            }
+        }
+    }
+}
